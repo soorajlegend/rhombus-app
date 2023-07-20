f8ab4689b78ea6d758613a9ca64f51370faf52b7
suraj muhammad <soorajwizard01@gmail.com>
2023-07-20 16:53:46 +0300

    resolved footer location

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-white border-t'>
+    <footer className='bg-white border-t min-h-[40vh] absolute bottom-0'>
         <div className="mx-auto py-10">
             <p className="text-center text-sm text-black">
                 &copy; {format(new Date(), "yyyy")} Rhombus, Inc. All right reserved.
